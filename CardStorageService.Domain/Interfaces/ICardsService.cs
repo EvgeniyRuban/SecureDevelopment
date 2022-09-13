5172acd379144878ba5ede87d5b17c37a9df6cd3
EvgeniyRuban <eugene_1997@inbox.ru>
2022-09-13 08:18:06 +0600

    Added summery comments.

--- CardStorageService.Domain/Interfaces/ICardsService.cs
+++ CardStorageService.Domain/Interfaces/ICardsService.cs
@@ -2,9 +2,24 @@
 
 public interface ICardsService
 {
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task<Guid> Add(CardToCreate cardToCreate, CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task<CardResponse> Get(Guid id, CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task<IEnumerable<CardResponse>> GetAll(CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task Update(CardToUpdate cardToUpdate, CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task Delete(Guid id, CancellationToken cancellationToken);
 }
--- CardStorageService.Domain/Interfaces/IClientsRepository.cs
+++ CardStorageService.Domain/Interfaces/IClientsRepository.cs
@@ -2,9 +2,24 @@
 
 public interface IClientsRepository
 {
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task<Guid> Add(ClientToCreate clientToCreate, CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task<ClientResponse> Get(Guid id, CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task<IEnumerable<ClientResponse>> GetAll(CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task Update(ClientToUpdate clientToUpdate, CancellationToken cancellationToken);
+    /// <summary></summary>
+    /// <exception cref="OperationCanceledException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
     Task Delete(Guid id, CancellationToken cancellationToken);
 }
